609d3374ba737e883771d09c269582f845539954
Joey Luis Echevarria <jechevarria719@gmail.com>
2019-09-26 19:25:32 +0000

    first commit

---
+++ lib/test/JOEYBK.rb
@@ -0,0 +1,8 @@
+require "test/JOEYBK/version"
+
+module Test
+  module JOEYBK
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/test/JOEYBK/version.rb
@@ -0,0 +1,5 @@
+module Test
+  module JOEYBK
+    VERSION = "0.1.0"
+  end
+end
